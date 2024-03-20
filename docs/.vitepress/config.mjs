2669a3eead353af5a172af88f90a09962ed48576
ArthurWang <legendaryarthur@163.com>
2024-03-20 09:22:22 +0800

    feat: css 404

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Front-End interviews",
   description: "fe interview",
-  base: './',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
