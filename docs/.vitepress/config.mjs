b7af72cc88c1fb1af96e4a98e91a1fe7be133b44
ArthurWang <legendaryarthur@163.com>
2024-03-23 10:58:10 +0800

    feat: favicon

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -8,8 +8,8 @@ export default defineConfig({
   // outDir: "../public",
   base: "/fe-interview/",
   head: [
-    // ['link', {rel: 'shortcut icon', href: '/fe-interview/favicon.ico'}]
-    ['link', {rel: 'icon', type: 'image/png', sizes: '32x32', href: '/fe-interview/favicon.png'}]
+    ['link', {rel: 'shortcut icon', href: '/fe-interview/favicon.ico?v=1.1'}]
+    // ['link', {rel: 'icon', type: 'image/png', sizes: '32x32', href: '/fe-interview/favicon.png'}]
   ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
